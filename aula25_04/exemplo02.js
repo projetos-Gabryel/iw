cf79acebc84f914218d6db085d851a0a08025190
Gabryel henrique sousa da silva <80103175+projetos-Gabryel@users.noreply.github.com>
2022-04-25 09:02:24 -0300

    Add files via upload

---
+++ aula25_04/exemplo02.js
@@ -0,0 +1,7 @@
+ var velocidade = 60
+ console.log(`A velocidade do carro é de ${velocidade} km/h`)
+ if(velocidade>=60){
+     console.log(`Você ultrapassou o limite de velocidade`)
+    console.log(`Será MULTADO!`)
+ }
+ console.log(`Dirija com cuidado`)
